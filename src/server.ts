fc925e3902671409909456caed0b685578b099ad
Natan Dourado <nsdourado12@gmail.com>
2025-07-09 17:40:45 -0300

    feat: implement CRUD operations for rooms and questions with corresponding routes

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,28 @@
-import fastifyCors from "@fastify/cors";
-import { fastify } from "fastify";
-import { serializerCompiler, validatorCompiler, type ZodTypeProvider} from "fastify-type-provider-zod";
-// import { sql } from '../src/db/connection.ts'
-import { env } from "./env.ts";
-import { getRoomsRoute } from "./http/routes/get-rooms.ts";
+import fastifyCors from '@fastify/cors'
+import { fastify } from 'fastify'
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
+import { env } from './env.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: "http://localhost:5173",
-});
+  origin: 'http://localhost:5173',
+})
 
-app.setSerializerCompiler(serializerCompiler);
-app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
 
-app.get("/health", () => {
-  return "ok";
-});
+app.get('/health', () => {
+  return 'ok'
+})
 
-app.register(getRoomsRoute);
+app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
+app.register(createQuestionRoute)
 
-app.listen({ port: env.PORT });
+app.listen({ port: env.PORT })
